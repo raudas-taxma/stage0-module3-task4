f97a99eb3327ff4f9656ee648cebcd649db9718e
rgsad <rsad3262@gmail.com>
2025-02-15 19:27:17 +0300

    solution

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char c = (char) 99;
+        char h = (char) 104;
+        char a = (char) 97;
+        char r = (char) 114;
+        System.out.println(c);
+        System.out.println(h);
+        System.out.println(a);
+        System.out.println(r);
     }
 }
